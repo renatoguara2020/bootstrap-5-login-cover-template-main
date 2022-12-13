92e101b11d49e85438938e1d1c1cd35f2f4d5bd2
Renato Alves Soares <renatoguara2020@gmail.com>
2022-12-13 19:58:24 -0300

    ok

---
+++ PDO_BINDVALUE.php
@@ -0,0 +1,26 @@
+<?php  
+$database = "AdventureWorks";  
+$server = "(local)";  
+$conn = new PDO("sqlsrv:server=$server ; Database = $database", "", "");  
+  
+$contact = "Sales Agent";  
+$stmt = $conn->prepare("select * from Person.ContactType where name = ?");  
+$stmt->bindValue(1, $contact);  
+$contact = "Owner";  
+$stmt->execute();  
+  
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
+   print "$row[Name]\n\n";  
+}  
+  
+$stmt = null;  
+$contact = "Sales Agent";  
+$stmt = $conn->prepare("select * from Person.ContactType where contact = :contact");  
+$stmt->bindValue(':contact', $contact);  
+$contact = "Owner";  
+$stmt->execute();  
+  
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {  
+   print "$row[Name]\n\n";  
+}  
+?>
